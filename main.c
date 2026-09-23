0ce9352f76dc0416413fb011101a76e3647a2c59
Minis Assistant <minis@local>
2026-09-23 13:55:46 -0600

    Move dump_wx_stats weak definition to global scope in main.c

--- main.c
+++ main.c
@@ -374,10 +374,12 @@ static void ish_cli_memory_governor_start(void) {
         pthread_detach(t);
 }
 
+__attribute__((weak)) void dump_wx_stats(void) {}
+
 int main(int argc, char *const argv[]) {
     ish_signpost_init();
     atexit(dump_pc_hist);
-    { __attribute__((weak)) void dump_wx_stats(void) {} atexit(dump_wx_stats); }
+    atexit(dump_wx_stats);
     // Write watchpoint (diagnostic): ISH_WATCH_PAGE=<hex page addr> arms the
     // per-store page check in the write gadgets; ISH_WATCH_LO/HI narrow which
     // hits get recorded.
